831b4b7473abc7eac0d5ace789c90d28184521e6
Deep9924 <91365008+Deep9924@users.noreply.github.com>
2026-04-05 08:12:51 -0600

    Update layout.tsx

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,7 +1,6 @@
 // src/app/account/layout.tsx
 "use client";
 
-import Navbar from "@/components/Navbar";
 import { useState } from "react";
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
@@ -9,10 +8,6 @@ export default function AccountLayout({ children }: { children: React.ReactNode 
 
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar
-        searchQuery={searchQuery}
-        onSearchChange={setSearchQuery}
-      />
       <main className="flex-1">{children}</main>
     </div>
   );
